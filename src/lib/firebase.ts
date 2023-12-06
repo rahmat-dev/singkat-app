c691c5b6c0fb3da13a25ea3d9676d0129f24944f
Rahmat Sulistio <rr7465457@gmail.com>
2023-12-06 23:38:52 +0700

    :wrench: Move firebase config to env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,15 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCiThpa46AF2O0G8j7PwMMWb_rimnKFdhs',
-  authDomain: 'singkat-app.firebaseapp.com',
-  projectId: 'singkat-app',
-  storageBucket: 'singkat-app.appspot.com',
-  messagingSenderId: '26856397370',
-  appId: '1:26856397370:web:6a740c17310a30efc5f303',
-  measurementId: 'G-3E24YK6B8Z',
-}
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
